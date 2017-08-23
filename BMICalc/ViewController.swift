20c7e570e14af88be1fdc53b0af170c17184cfbd
John Fansler <john.fansler@gmail.com>
2017-08-23 07:52:59 -0700

    Basic BMI Calculator

--- BMICalc/ViewController.swift
+++ BMICalc/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var weightText: UITextField!
+    @IBOutlet weak var heightText: UITextField!
+    @IBOutlet weak var BMILabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        print("pressed")
+        print("\(weightText.text!)")
+        print(heightText.text!)
+        if let height = Double(heightText.text!) {
+            if let weight = Double(weightText.text!) {
+                var bmiValue = (weight * 0.45) / pow(height * 0.025,2.0)
+                print(bmiValue);
+                BMILabel.text = String(Int(bmiValue))
+            } else {
+                let alertController = UIAlertController(title: "Error", message: "Invalid weight entry", preferredStyle: .alert)
+                let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(OKAction)
+                self.present(alertController, animated: true, completion: nil)
+                weightText.text=""
+            }
+        } else {
+            let alertController = UIAlertController(title: "Error", message: "Invalid height entry", preferredStyle: .alert)
+            let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(OKAction)
+            self.present(alertController, animated: true, completion: nil)
+            heightText.text=""
+        }
+        
     }
-
-
+    
+    
 }
 
